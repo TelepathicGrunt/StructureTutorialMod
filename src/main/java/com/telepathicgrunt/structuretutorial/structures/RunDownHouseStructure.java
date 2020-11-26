2c253a848a598b6973bc8da0a4a66c0430a714fd
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2020-11-26 10:26:00 -0500

    improved comments to clarify the file paths

--- src/main/java/com/telepathicgrunt/structuretutorial/structures/RunDownHouseStructure.java
+++ src/main/java/com/telepathicgrunt/structuretutorial/structures/RunDownHouseStructure.java
@@ -134,6 +134,12 @@ public class RunDownHouseStructure extends Structure<NoFeatureConfig> {
                     dynamicRegistryManager,
                     new VillageConfig(() -> dynamicRegistryManager.getRegistry(Registry.JIGSAW_POOL_KEY)
                             // The path to the starting Template Pool JSON file to read.
+                            //
+                            // Note, this is "structure_tutorial:run_down_house/start_pool" which means
+                            // the game will automatically look into the following path for the template pool:
+                            // "resources/data/structure_tutorial/worldgen/template_pool/run_down_house/start_pool.json"
+                            // This is why your pool files must be in "data/<modid>/worldgen/template_pool/<the path to the pool here>"
+                            // because the game automatically will check in worldgen/template_pool for the pools.
                             .getOrDefault(new ResourceLocation(StructureTutorialMain.MODID, "run_down_house/start_pool")),
 
                             // How many pieces outward from center can a recursive jigsaw structure spawn.
