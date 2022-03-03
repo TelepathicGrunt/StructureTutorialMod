9abbe05b7d5ed17b99d37370e4973d88f7851391
TelepathicGrunt <alexsarnese@gmail.com>
2022-03-02 21:32:14 -0500

    updated to 1.18.2

--- src/main/java/com/telepathicgrunt/structuretutorial/STStructures.java
+++ src/main/java/com/telepathicgrunt/structuretutorial/STStructures.java
@@ -1,19 +1,11 @@
 package com.telepathicgrunt.structuretutorial;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.telepathicgrunt.structuretutorial.structures.RunDownHouseStructure;
-import net.minecraft.data.BuiltinRegistries;
-import net.minecraft.world.level.levelgen.StructureSettings;
+import com.telepathicgrunt.structuretutorial.structures.SkyStructures;
 import net.minecraft.world.level.levelgen.feature.StructureFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.JigsawConfiguration;
-import net.minecraft.world.level.levelgen.feature.configurations.StructureFeatureConfiguration;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class STStructures {
 
@@ -28,119 +20,9 @@ public class STStructures {
     public static final DeferredRegister<StructureFeature<?>> DEFERRED_REGISTRY_STRUCTURE = DeferredRegister.create(ForgeRegistries.STRUCTURE_FEATURES, StructureTutorialMain.MODID);
 
     /**
-     * Registers the structure itself and sets what its path is. In this case, the
-     * structure will have the resourcelocation of structure_tutorial:run_down_house.
-     *
-     * It is always a good idea to register your Structures so that other mods and datapacks can
-     * use them too directly from the registries. It great for mod/datapacks compatibility.
-     *
-     * IMPORTANT: Once you have set the name for your structure below and distributed your mod,
-     *   changing the structure's registry name or removing the structure may cause log spam.
-     *   This log spam won't break your worlds as forge already fixed the Mojang bug of removed structures wrecking worlds.
-     *   https://github.com/MinecraftForge/MinecraftForge/commit/56e538e8a9f1b8e6ff847b9d2385484c48849b8d
-     *
-     *   However, users might not know that and think you are to blame for issues that doesn't exist.
-     *   So it is best to keep your structure names the same as long as you can instead of changing them frequently.
+     * Registers the base structure itself and sets what its path is. In this case,
+     * this base structure will have the resourcelocation of structure_tutorial:sky_structures.
      */
-    public static final RegistryObject<StructureFeature<JigsawConfiguration>> RUN_DOWN_HOUSE = DEFERRED_REGISTRY_STRUCTURE.register("run_down_house", () -> (new RunDownHouseStructure(JigsawConfiguration.CODEC)));
+    public static final RegistryObject<StructureFeature<?>> SKY_STRUCTURES = DEFERRED_REGISTRY_STRUCTURE.register("sky_structures", SkyStructures::new);
 
-    /**
-     * This is where we set the rarity of your structures and determine if land conforms to it.
-     * See the comments in below for more details.
-     */
-    public static void setupStructures() {
-        setupMapSpacingAndLand(
-                RUN_DOWN_HOUSE.get(), /* The instance of the structure */
-                new StructureFeatureConfiguration(10 /* average distance apart in chunks between spawn attempts */,
-                        5 /* minimum distance apart in chunks between spawn attempts. MUST BE LESS THAN ABOVE VALUE*/,
-                        1234567890 /* this modifies the seed of the structure so no two structures always spawn over each-other. Make this large and unique. */),
-                true);
-
-
-        // Add more structures here and so on
-    }
-
-    /**
-     * Adds the provided structure to the registry, and adds the separation settings.
-     * The rarity of the structure is determined based on the values passed into
-     * this method in the StructureFeatureConfiguration argument.
-     * This method is called by setupStructures above.
-     */
-    public static <F extends StructureFeature<?>> void setupMapSpacingAndLand(
-            F structure,
-            StructureFeatureConfiguration structureFeatureConfiguration,
-            boolean transformSurroundingLand)
-    {
-        /*
-         * We need to add our structures into the map in StructureFeature class
-         * alongside vanilla structures or else it will cause errors.
-         *
-         * If the registration is setup properly for the structure,
-         * getRegistryName() should never return null.
-         */
-        StructureFeature.STRUCTURES_REGISTRY.put(structure.getRegistryName().toString(), structure);
-
-        /*
-         * Whether surrounding land will be modified automatically to conform to the bottom of the structure.
-         * Basically, it adds land at the base of the structure like it does for Villages and Outposts.
-         * Doesn't work well on structure that have pieces stacked vertically or change in heights.
-         *
-         * Note: The air space this method will create will be filled with water if the structure is below sealevel.
-         * This means this is best for structure above sealevel so keep that in mind.
-         *
-         * NOISE_AFFECTING_FEATURES requires AccessTransformer  (See resources/META-INF/accesstransformer.cfg)
-         */
-        if(transformSurroundingLand){
-            StructureFeature.NOISE_AFFECTING_FEATURES =
-                    ImmutableList.<StructureFeature<?>>builder()
-                            .addAll(StructureFeature.NOISE_AFFECTING_FEATURES)
-                            .add(structure)
-                            .build();
-        }
-
-        /*
-         * This is the map that holds the default spacing of all structures.
-         * Always add your structure to here so that other mods can utilize it if needed.
-         *
-         * However, while it does propagate the spacing to some correct dimensions from this map,
-         * it seems it doesn't always work for code made dimensions as they read from this list beforehand.
-         *
-         * Instead, we will use the WorldEvent.Load event in StructureTutorialMain to add the structure
-         * spacing from this list into that dimension or to do dimension blacklisting properly.
-         *
-         * DEFAULTS requires AccessTransformer  (See resources/META-INF/accesstransformer.cfg)
-         */
-        StructureSettings.DEFAULTS =
-                ImmutableMap.<StructureFeature<?>, StructureFeatureConfiguration>builder()
-                        .putAll(StructureSettings.DEFAULTS)
-                        .put(structure, structureFeatureConfiguration)
-                        .build();
-
-
-        /*
-         * There are very few mods that relies on seeing your structure in the noise settings registry before the world is made.
-         *
-         * You may see some mods add their spacings to DimensionSettings.BUILTIN_OVERWORLD instead of the NOISE_GENERATOR_SETTINGS loop below but
-         * that field only applies for the default overworld and won't add to other worldtypes or dimensions (like amplified or Nether).
-         * So yeah, don't do DimensionSettings.BUILTIN_OVERWORLD. Use the NOISE_GENERATOR_SETTINGS loop below instead if you must.
-         */
-        BuiltinRegistries.NOISE_GENERATOR_SETTINGS.entrySet().forEach(settings -> {
-            Map<StructureFeature<?>, StructureFeatureConfiguration> structureMap = settings.getValue().structureSettings().structureConfig();
-
-            /*
-             * Pre-caution in case a mod makes the structure map immutable like datapacks do.
-             * I take no chances myself. You never know what another mods does...
-             *
-             * structureConfig requires AccessTransformer (See resources/META-INF/accesstransformer.cfg)
-             */
-            if(structureMap instanceof ImmutableMap){
-                Map<StructureFeature<?>, StructureFeatureConfiguration> tempMap = new HashMap<>(structureMap);
-                tempMap.put(structure, structureFeatureConfiguration);
-                settings.getValue().structureSettings().structureConfig = tempMap;
-            }
-            else{
-                structureMap.put(structure, structureFeatureConfiguration);
-            }
-        });
-    }
 }
---
+++ src/main/java/com/telepathicgrunt/structuretutorial/structures/SkyStructures.java
@@ -0,0 +1,119 @@
+package com.telepathicgrunt.structuretutorial.structures;
+
+import com.telepathicgrunt.structuretutorial.StructureTutorialMain;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.levelgen.feature.StructureFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.JigsawConfiguration;
+import net.minecraft.world.level.levelgen.structure.BuiltinStructureSets;
+import net.minecraft.world.level.levelgen.structure.PoolElementStructurePiece;
+import net.minecraft.world.level.levelgen.structure.PostPlacementProcessor;
+import net.minecraft.world.level.levelgen.structure.pieces.PieceGenerator;
+import net.minecraft.world.level.levelgen.structure.pieces.PieceGeneratorSupplier;
+import net.minecraft.world.level.levelgen.structure.pools.JigsawPlacement;
+import org.apache.logging.log4j.Level;
+
+import java.util.Optional;
+
+public class SkyStructures extends StructureFeature<JigsawConfiguration> {
+
+    public SkyStructures() {
+        // Create the pieces layout of the structure and give it to the game
+        super(JigsawConfiguration.CODEC, SkyStructures::createPiecesGenerator, PostPlacementProcessor.NONE);
+    }
+
+    /**
+     *        : WARNING!!! DO NOT FORGET THIS METHOD!!!! :
+     * If you do not override step method, your structure WILL crash the biome as it is being parsed!
+     *
+     * Generation step for when to generate the structure. there are 10 stages you can pick from!
+     * This surface structure stage places the structure before plants and ores are generated.
+     */
+    @Override
+    public GenerationStep.Decoration step() {
+        return GenerationStep.Decoration.SURFACE_STRUCTURES;
+    }
+
+    /*
+     * This is where extra checks can be done to determine if the structure can spawn here.
+     * This only needs to be overridden if you're adding additional spawn conditions.
+     *
+     * Fun fact, if you set your structure separation/spacing to be 0/1, you can use
+     * isFeatureChunk to return true only if certain chunk coordinates are passed in
+     * which allows you to spawn structures only at certain coordinates in the world.
+     *
+     * Basically, this method is used for determining if the land is at a suitable height,
+     * if certain other structures are too close or not, or some other restrictive condition.
+     *
+     * For example, Pillager Outposts added a check to make sure it cannot spawn within 10 chunk of a Village.
+     * (Bedrock Edition seems to not have the same check)
+     *
+     * If you are doing Nether structures, you'll probably want to spawn your structure on top of ledges.
+     * Best way to do that is to use getBaseColumn to grab a column of blocks at the structure's x/z position.
+     * Then loop through it and look for land with air above it and set blockpos's Y value to it.
+     * Make sure to set the final boolean in JigsawPlacement.addPieces to false so
+     * that the structure spawns at blockpos's y value instead of placing the structure on the Bedrock roof!
+     *
+     * Also, please for the love of god, do not do dimension checking here.
+     * If you do and another mod's dimension is trying to spawn your structure,
+     * the locate command will make minecraft hang forever and break the game.
+     * Use the biome tags for where to spawn the structure and users can datapack
+     * it to spawn in specific biomes that aren't in the dimension they don't like if they wish.
+     */
+    private static boolean isFeatureChunk(PieceGeneratorSupplier.Context<JigsawConfiguration> context) {
+        // Grabs the chunk position we are at
+        ChunkPos chunkpos = context.chunkPos();
+
+        // Checks to make sure our structure does not spawn within 10 chunks of an Ocean Monument
+        // to demonstrate how this method is good for checking extra conditions for spawning
+        return !context.chunkGenerator().hasFeatureChunkInRange(BuiltinStructureSets.OCEAN_MONUMENTS, context.seed(), chunkpos.x, chunkpos.z, 10);
+    }
+
+    public static Optional<PieceGenerator<JigsawConfiguration>> createPiecesGenerator(PieceGeneratorSupplier.Context<JigsawConfiguration> context) {
+
+        // Check if the spot is valid for our structure. This is just as another method for cleanness.
+        // Returning an empty optional tells the game to skip this spot as it will not generate the structure.
+        if (!SkyStructures.isFeatureChunk(context)) {
+            return Optional.empty();
+        }
+
+        // Turns the chunk coordinates into actual coordinates we can use. (Gets center of that chunk)
+        BlockPos blockpos = context.chunkPos().getMiddleBlockPosition(0);
+
+        // Find the top Y value of the land and then offset our structure to 60 blocks above that.
+        // WORLD_SURFACE_WG will stop at top water so we don't accidentally put our structure into the ocean if it is a super deep ocean.
+        int topLandY = context.chunkGenerator().getFirstFreeHeight(blockpos.getX(), blockpos.getZ(), Heightmap.Types.WORLD_SURFACE_WG, context.heightAccessor());
+        blockpos = blockpos.above(topLandY + 60);
+
+        Optional<PieceGenerator<JigsawConfiguration>> structurePiecesGenerator =
+                JigsawPlacement.addPieces(
+                        context, // Used for JigsawPlacement to get all the proper behaviors done.
+                        PoolElementStructurePiece::new, // Needed in order to create a list of jigsaw pieces when making the structure's layout.
+                        blockpos, // Position of the structure. Y value is ignored if last parameter is set to true.
+                        false,  // Special boundary adjustments for villages. It's... hard to explain. Keep this false and make your pieces not be partially intersecting.
+                        // Either not intersecting or fully contained will make children pieces spawn just fine. It's easier that way.
+                        false // Place at heightmap (top land). Set this to false for structure to be place at the passed in blockpos's Y value instead.
+                        // Definitely keep this false when placing structures in the nether as otherwise, heightmap placing will put the structure on the Bedrock roof.
+                );
+
+        /*
+         * Note, you are always free to make your own JigsawPlacement class and implementation of how the structure
+         * should generate. It is tricky but extremely powerful if you are doing something that vanilla's jigsaw system cannot do.
+         * Such as for example, forcing 3 pieces to always spawn every time, limiting how often a piece spawns, or remove the intersection limitation of pieces.
+         *
+         * An example of a custom JigsawPlacement.addPieces in action can be found here (warning, it is using Mojmap mappings):
+         * https://github.com/TelepathicGrunt/RepurposedStructures/blob/1.18.2/src/main/java/com/telepathicgrunt/repurposedstructures/world/structures/pieces/PieceLimitedJigsawManager.java
+         */
+
+        if(structurePiecesGenerator.isPresent()) {
+            // I use to debug and quickly find out if the structure is spawning or not and where it is.
+            // This is returning the coordinates of the center starting piece.
+            StructureTutorialMain.LOGGER.log(Level.DEBUG, "Rundown House at {}", blockpos);
+        }
+
+        // Return the pieces generator that is now set up so that the game runs it when it needs to create the layout of structure pieces.
+        return structurePiecesGenerator;
+    }
+}
