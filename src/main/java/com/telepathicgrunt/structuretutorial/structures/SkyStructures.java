1150eed5ea602cccf9d72611e9155b264fcac393
TelepathicGrunt <alexsarnese@gmail.com>
2022-05-12 18:32:19 -0400

    add more info on heightmap spawning field

--- src/main/java/com/telepathicgrunt/structuretutorial/structures/SkyStructures.java
+++ src/main/java/com/telepathicgrunt/structuretutorial/structures/SkyStructures.java
@@ -94,10 +94,9 @@ public class SkyStructures extends StructureFeature<JigsawConfiguration> {
         // Turns the chunk coordinates into actual coordinates we can use. (Gets center of that chunk)
         BlockPos blockpos = context.chunkPos().getMiddleBlockPosition(0);
 
-        // Find the top Y value of the land and then offset our structure to 60 blocks above that.
-        // WORLD_SURFACE_WG will stop at top water so we don't accidentally put our structure into the ocean if it is a super deep ocean.
-        int topLandY = context.chunkGenerator().getFirstFreeHeight(blockpos.getX(), blockpos.getZ(), Heightmap.Types.WORLD_SURFACE_WG, context.heightAccessor());
-        blockpos = blockpos.above(topLandY + 60);
+        // Set's our spawning blockpos's y offset to be 60 blocks up.
+        // Since we are going to have heightmap/terrain height spawning set to true further down, this will make it so we spawn 60 blocks above terrain.
+        blockpos = blockpos.above(60);
 
         Optional<PieceGenerator<JigsawConfiguration>> structurePiecesGenerator =
                 JigsawPlacement.addPieces(
@@ -106,7 +105,9 @@ public class SkyStructures extends StructureFeature<JigsawConfiguration> {
                         blockpos, // Position of the structure. Y value is ignored if last parameter is set to true.
                         false,  // Special boundary adjustments for villages. It's... hard to explain. Keep this false and make your pieces not be partially intersecting.
                         // Either not intersecting or fully contained will make children pieces spawn just fine. It's easier that way.
-                        false // Place at heightmap (top land). Set this to false for structure to be place at the passed in blockpos's Y value instead.
+                        true // Adds the terrain height's y value to the passed in blockpos's y value. (This uses WORLD_SURFACE_WG heightmap which stops at top water too)
+                        // Here, blockpos's y value is 60 which means the structure spawn 60 blocks above terrain height.
+                        // Set this to false for structure to be place only at the passed in blockpos's Y value instead.
                         // Definitely keep this false when placing structures in the nether as otherwise, heightmap placing will put the structure on the Bedrock roof.
                 );
 
