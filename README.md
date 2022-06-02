bed7f02d9e3155f4d1f7d11df798c7899027dc6e
TelepathicGrunt <alexsarnese@gmail.com>
2022-06-01 21:01:37 -0400

    ported to 1.19

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Structure Tutorial Mod (Quilt)
-**How to register and generate jigsaw structures in 1.18.2+ Minecraft Quilt using nbt files! In fact, you can actually make structures using only json files in 1.18.2+ MC!**
+**How to register and generate jigsaw structures in 1.19.0+ Minecraft Quilt using nbt files! In fact, you can actually make structures using only json files in 1.18.2+ MC!**
 
 **Change the Github branch to see other versions of this tutorial including Forge.**
 
--- src/main/java/com/telepathicgrunt/structure_tutorial/STStructures.java
+++ src/main/java/com/telepathicgrunt/structure_tutorial/STStructures.java
@@ -1,27 +1,30 @@
 package com.telepathicgrunt.structure_tutorial;
 
-import com.telepathicgrunt.structure_tutorial.mixin.StructureFeatureAccessor;
+import com.mojang.serialization.Codec;
 import com.telepathicgrunt.structure_tutorial.structures.SkyStructures;
+import net.minecraft.structure.Structure;
+import net.minecraft.structure.StructureType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.feature.StructureFeature;
 
 public class STStructures {
 
-    /**
+    public static StructureType<?> SKY_STRUCTURES;
+
     /**
      * Registers the structure itself and sets what its path is. In this case, the
      * structure will have the Identifier of structure_tutorial:sky_structures.
      *
      * It is always a good idea to register your Structures so that other mods and datapacks can
-     * use them too directly from the registries. It great for mod/datapacks compatibility.
+     * use them too directly from the registries. It's great for mod/datapacks compatibility.
      */
-    public static StructureFeature<?> SKY_STRUCTURES = new SkyStructures();
-
     public static void registerStructureFeatures() {
-        // The generation step for when to generate the structure. there are 10 stages you can pick from!
-        // This surface structure stage places the structure before plants and ores are generated.
-        StructureFeatureAccessor.callRegister(StructureTutorialMain.MODID + ":sky_structures", SKY_STRUCTURES, GenerationStep.Feature.SURFACE_STRUCTURES);
+        SKY_STRUCTURES = register(new Identifier(StructureTutorialMain.MODID, "sky_structures"), SkyStructures.CODEC);
+    }
+
+    // Helper method to register since compiler will complain about typing if we did () -> SkyStructures.CODEC directly.
+    private static <S extends StructureFeature> StructureType<S> register(Identifier id, Codec<S> codec) {
+        return Registry.register(Registry.STRUCTURE_TYPE, id, () -> codec);
     }
 }
