8cd20e4bbe519338bf3d1222345b16985844363c
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-10-21 23:54:17 -0400

    address double lambda typing issues

--- src/main/java/com/telepathicgrunt/structuretutorial/STStructures.java
+++ src/main/java/com/telepathicgrunt/structuretutorial/STStructures.java
@@ -1,7 +1,9 @@
 package com.telepathicgrunt.structuretutorial;
 
+import com.mojang.serialization.Codec;
 import com.telepathicgrunt.structuretutorial.structures.SkyStructures;
 import net.minecraft.core.Registry;
+import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
@@ -18,5 +20,14 @@ public class STStructures {
      * Registers the base structure itself and sets what its path is. In this case,
      * this base structure will have the resourcelocation of structure_tutorial:sky_structures.
      */
-    public static final RegistryObject<StructureType<SkyStructures>> SKY_STRUCTURES = DEFERRED_REGISTRY_STRUCTURE.register("sky_structures", () -> () -> SkyStructures.CODEC);
+    public static final RegistryObject<StructureType<SkyStructures>> SKY_STRUCTURES = DEFERRED_REGISTRY_STRUCTURE.register("sky_structures", () -> explicitStructureTypeTyping(SkyStructures.CODEC));
+
+    /**
+     * Originally, I had a double lambda ()->()-> for the RegistryObject line above, but it turns out that
+     * some IDEs cannot resolve the typing correctly. This method explicitly states what the return type
+     * is so that the IDE can put it into the DeferredRegistry properly.
+     */
+    private static <T extends Structure> StructureType<T> explicitStructureTypeTyping(Codec<T> structureCodec) {
+        return () -> structureCodec;
+    }
 }
