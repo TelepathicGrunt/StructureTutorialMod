67b4787b35c4e07671bd90e4c7de8f13a01e2c2d
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-12-15 18:19:11 -0500

    updated to 1.19.3

--- src/main/java/com/telepathicgrunt/structuretutorial/STStructures.java
+++ src/main/java/com/telepathicgrunt/structuretutorial/STStructures.java
@@ -3,6 +3,7 @@ package com.telepathicgrunt.structuretutorial;
 import com.mojang.serialization.Codec;
 import com.telepathicgrunt.structuretutorial.structures.SkyStructures;
 import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureType;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,7 +15,7 @@ public class STStructures {
      * We are using the Deferred Registry system to register our structure as this is the preferred way on Forge.
      * This will handle registering the base structure for us at the correct time so we don't have to handle it ourselves.
      */
-    public static final DeferredRegister<StructureType<?>> DEFERRED_REGISTRY_STRUCTURE = DeferredRegister.create(Registry.STRUCTURE_TYPE_REGISTRY, StructureTutorialMain.MODID);
+    public static final DeferredRegister<StructureType<?>> DEFERRED_REGISTRY_STRUCTURE = DeferredRegister.create(Registries.STRUCTURE_TYPE, StructureTutorialMain.MODID);
 
     /**
      * Registers the base structure itself and sets what its path is. In this case,
