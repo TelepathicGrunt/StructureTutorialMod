396472a69d5ffc4338afe65034ce1ba078ae8ecb
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-05-02 07:18:05 -0400

    Update to 1.20.4

--- src/main/java/com/telepathicgrunt/structure_tutorial/structures/SkyStructures.java
+++ src/main/java/com/telepathicgrunt/structure_tutorial/structures/SkyStructures.java
@@ -6,6 +6,7 @@ import com.telepathicgrunt.structure_tutorial.STStructures;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.structure.pool.StructurePool;
 import net.minecraft.structure.pool.StructurePoolBasedGenerator;
+import net.minecraft.structure.pool.alias.StructurePoolAliasLookup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
@@ -125,7 +126,8 @@ public class SkyStructures extends Structure {
                         // Here, blockpos's y value is 60 which means the structure spawn 60 blocks above terrain height.
                         // Set this to false for structure to be place only at the passed in blockpos's Y value instead.
                         // Definitely keep this false when placing structures in the nether as otherwise, heightmap placing will put the structure on the Bedrock roof.
-                    this.maxDistanceFromCenter); // Maximum limit for how far pieces can spawn from center. You cannot set this bigger than 128 or else pieces gets cutoff.
+                    this.maxDistanceFromCenter, // Maximum limit for how far pieces can spawn from center. You cannot set this bigger than 128 or else pieces gets cutoff.
+                    StructurePoolAliasLookup.EMPTY); // Optional thing that allows swapping a template pool with another per structure json instance. We don't need this but see vanilla JigsawStructure class for how to wire it up if you want it.
 
         /*
          * Note, you are always free to make your own StructurePoolBasedGenerator class and implementation of how the structure
