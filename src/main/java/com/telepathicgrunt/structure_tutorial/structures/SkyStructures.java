0d3c44066a300c6bda96d761e223f824e9d1ddb8
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2025-04-09 23:14:09 -0400

    port code to work on fabric

--- src/main/java/com/telepathicgrunt/structure_tutorial/structures/SkyStructures.java
+++ src/main/java/com/telepathicgrunt/structure_tutorial/structures/SkyStructures.java
@@ -122,9 +122,9 @@ public class SkyStructures extends Structure {
                     blockPos, // Where to spawn the structure.
                     false, // "useExpansionHack" This is for legacy villages to generate properly. You should keep this false always.
                     this.projectStartToHeightmap, // Adds the terrain height's y value to the passed in blockpos's y value. (This uses WORLD_SURFACE_WG heightmap which stops at top water too)
-                        // Here, blockpos's y value is 60 which means the structure spawn 60 blocks above terrain height.
-                        // Set this to false for structure to be place only at the passed in blockpos's Y value instead.
-                        // Definitely keep this false when placing structures in the nether as otherwise, heightmap placing will put the structure on the Bedrock roof.
+                        // Here at projectStartToHeightmap, blockpos's y value is 60 which means the structure spawn 60 blocks above terrain height.
+                        // Set projectStartToHeightmap to be empty optional for structure to be place only at the passed in blockpos's Y value instead.
+                        // Definitely keep this an empty optional when placing structures in the nether as otherwise, heightmap placing will put the structure on the Bedrock roof.
                     this.maxDistanceFromCenter); // Maximum limit for how far pieces can spawn from center. You cannot set this bigger than 128 or else pieces gets cutoff.
 
         /*
---
+++ src/main/java/com/telepathicgrunt/structure_tutorial/utilities/FilterHolderSet.java
@@ -0,0 +1,136 @@
+package com.telepathicgrunt.structure_tutorial.utilities;
+
+import com.mojang.datafixers.util.Either;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.entry.RegistryEntryList;
+import net.minecraft.registry.entry.RegistryEntryListCodec;
+import net.minecraft.registry.entry.RegistryEntryOwner;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.math.random.Random;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * A special HolderSet that holds one HolderSet as the base entries and uses a second HolderSet as a filter on those entries.
+ * You can copy and paste this to your project. You do not need to make any changes here.
+ * Simply use this in your structure's codec to replace the biomes HolderSet field. See OceanStructures.java for an example of usage.
+ */
+public class FilterHolderSet<T> implements RegistryEntryList<T> {
+    public static <T> MapCodec<FilterHolderSet<T>> codec(RegistryKey<? extends Registry<T>> registryKey, Codec<RegistryEntry<T>> holderCodec, boolean forceList) {
+        return RecordCodecBuilder.mapCodec(
+                builder -> builder
+                        .group(
+                                RegistryEntryListCodec.create(registryKey, holderCodec, forceList).fieldOf("base").forGetter(FilterHolderSet::base),
+                                RegistryEntryListCodec.create(registryKey, holderCodec, forceList).fieldOf("filter").forGetter(FilterHolderSet::filter))
+                        .apply(builder, FilterHolderSet::new));
+    }
+
+    private final RegistryEntryList<T> base;
+    private final RegistryEntryList<T> filter;
+
+    private Set<RegistryEntry<T>> set = null;
+    private List<RegistryEntry<T>> list = null;
+
+    public RegistryEntryList<T> base() {
+        return this.base;
+    }
+    public RegistryEntryList<T> filter() {
+        return this.filter;
+    }
+
+    public FilterHolderSet(RegistryEntryList<T> base, RegistryEntryList<T> filter) {
+        this.base = base;
+        this.filter = filter;
+    }
+
+    /**
+     * {@return immutable Set of filtered Holders}
+     */
+    protected Set<RegistryEntry<T>> createSet() {
+        return this.base
+                .stream()
+                .filter(holder -> !this.filter.contains(holder))
+                .collect(Collectors.toSet());
+    }
+
+    public Set<RegistryEntry<T>> getSet() {
+        Set<RegistryEntry<T>> thisSet = this.set;
+        if (thisSet == null) {
+            Set<RegistryEntry<T>> set = this.createSet();
+            this.set = set;
+            return set;
+        } else {
+            return thisSet;
+        }
+    }
+
+    public List<RegistryEntry<T>> getList() {
+        List<RegistryEntry<T>> thisList = this.list;
+        if (thisList == null) {
+            List<RegistryEntry<T>> list = List.copyOf(this.getSet());
+            this.list = list;
+            return list;
+        } else {
+            return thisList;
+        }
+    }
+
+    @Override
+    public Stream<RegistryEntry<T>> stream() {
+        return this.getList().stream();
+    }
+
+    @Override
+    public int size() {
+        return this.getList().size();
+    }
+
+    @Override
+    public Either<TagKey<T>, List<RegistryEntry<T>>> getStorage() {
+        return Either.right(this.getList());
+    }
+
+    @Override
+    public Optional<RegistryEntry<T>> getRandom(Random rand) {
+        List<RegistryEntry<T>> list = this.getList();
+        int size = list.size();
+        return size > 0
+                ? Optional.of(list.get(rand.nextInt(size)))
+                : Optional.empty();
+    }
+
+    @Override
+    public RegistryEntry<T> get(int i) {
+        return this.getList().get(i);
+    }
+
+    @Override
+    public boolean contains(RegistryEntry<T> holder) {
+        return this.getSet().contains(holder);
+    }
+
+    @Override
+    public boolean ownerEquals(RegistryEntryOwner<T> holderOwner) {
+        return this.base.ownerEquals(holderOwner) && this.filter.ownerEquals(holderOwner);
+    }
+
+    @Override
+    public Optional<TagKey<T>> getTagKey() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Iterator<RegistryEntry<T>> iterator() {
+        return this.getList().iterator();
+    }
+}
