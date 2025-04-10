df1b0484a42cdf068600e6c48848f5a949067f07
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2025-04-09 23:14:09 -0400

    port code to work on fabric

--- src/main/java/com/telepathicgrunt/structure_tutorial/STStructures.java
+++ src/main/java/com/telepathicgrunt/structure_tutorial/STStructures.java
@@ -1,5 +1,6 @@
 package com.telepathicgrunt.structure_tutorial;
 
+import com.telepathicgrunt.structure_tutorial.structures.OceanStructures;
 import com.telepathicgrunt.structure_tutorial.structures.SkyStructures;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -9,6 +10,7 @@ import net.minecraft.world.gen.structure.StructureType;
 public class STStructures {
 
     public static StructureType<SkyStructures> SKY_STRUCTURES;
+    public static StructureType<OceanStructures> OCEAN_STRUCTURES;
 
     /**
      * Registers the structure itself and sets what its path is. In this case, the
@@ -19,5 +21,6 @@ public class STStructures {
      */
     public static void registerStructureFeatures() {
         SKY_STRUCTURES = Registry.register(Registries.STRUCTURE_TYPE, Identifier.of(StructureTutorialMain.MODID, "sky_structures"), () -> SkyStructures.CODEC);
+        OCEAN_STRUCTURES = Registry.register(Registries.STRUCTURE_TYPE, Identifier.of(StructureTutorialMain.MODID, "ocean_structures"), () -> OceanStructures.CODEC);
     }
 }
