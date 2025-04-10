ac87de9bd05717392ff358b7cef99ebbeeecd1a8
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2025-04-10 19:19:28 -0400

    Fix backport of 2 tag system

--- src/main/java/com/telepathicgrunt/structure_tutorial/structures/OceanStructures.java
+++ src/main/java/com/telepathicgrunt/structure_tutorial/structures/OceanStructures.java
@@ -5,19 +5,14 @@ import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.telepathicgrunt.structure_tutorial.STStructures;
 import com.telepathicgrunt.structure_tutorial.utilities.FilterHolderSet;
-import net.fabricmc.fabric.impl.biome.modification.BuiltInRegistryKeys;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.SpawnGroup;
-import net.minecraft.registry.BuiltinRegistries;
-import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.registry.entry.RegistryEntryList;
 import net.minecraft.registry.tag.FluidTags;
-import net.minecraft.structure.StructureLiquidSettings;
 import net.minecraft.structure.pool.StructurePool;
 import net.minecraft.structure.pool.StructurePoolBasedGenerator;
-import net.minecraft.structure.pool.alias.StructurePoolAliasLookup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.StringIdentifiable;
 import net.minecraft.util.math.BlockPos;
@@ -30,11 +25,10 @@ import net.minecraft.world.gen.HeightContext;
 import net.minecraft.world.gen.StructureTerrainAdaptation;
 import net.minecraft.world.gen.chunk.VerticalBlockSample;
 import net.minecraft.world.gen.heightprovider.HeightProvider;
-import net.minecraft.world.gen.structure.DimensionPadding;
-import net.minecraft.world.gen.structure.JigsawStructure;
 import net.minecraft.world.gen.structure.Structure;
 import net.minecraft.world.gen.structure.StructureType;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -44,7 +38,7 @@ public class OceanStructures extends Structure {
     public static final MapCodec<Structure.Config> CUSTOM_STRUCTURE_SETTINGS_CODEC = RecordCodecBuilder.mapCodec(
             codecBuilder -> codecBuilder.group(
                             // This is where we swapped in our custom codec that will apply the exclude structure tag to remove entries from the has structure tag.
-                            FilterHolderSet.codec(RegistryKeys.BIOME, Biome.REGISTRY_CODEC, false).fieldOf("biomes").forGetter(x -> x.biomes() instanceof FilterHolderSet<Biome> filterHolderSet ? filterHolderSet : new FilterHolderSet<>(x.biomes(), RegistryEntryList.empty())),
+                            FilterHolderSet.codec(RegistryKeys.BIOME, Biome.REGISTRY_CODEC, false).fieldOf("biomes").forGetter(x -> x.biomes() instanceof FilterHolderSet<Biome> filterHolderSet ? filterHolderSet : new FilterHolderSet<>(x.biomes(), RegistryEntryList.of(List.of()))),
                             Codec.simpleMap(SpawnGroup.CODEC, StructureSpawns.CODEC, StringIdentifiable.toKeyable(SpawnGroup.values()))
                                     .fieldOf("spawn_overrides")
                                     .forGetter(Structure.Config::spawnOverrides),
@@ -58,16 +52,14 @@ public class OceanStructures extends Structure {
 
     // A custom codec that changes the size limit for our code_structure_sea_boat.json's config to not be capped at 7.
     // With this, we can have a structure with a size limit up to 30 if we want to have extremely long branches of pieces in the structure.
-    public static final MapCodec<OceanStructures> CODEC = RecordCodecBuilder.mapCodec(instance ->
+    public static final Codec<OceanStructures> CODEC = RecordCodecBuilder.create(instance ->
             instance.group(CUSTOM_STRUCTURE_SETTINGS_CODEC.forGetter(structureInfo -> structureInfo.config),
                     StructurePool.REGISTRY_CODEC.fieldOf("start_pool").forGetter(structure -> structure.startPool),
                     Identifier.CODEC.optionalFieldOf("start_jigsaw_name").forGetter(structure -> structure.startJigsawName),
                     Codec.intRange(0, 30).fieldOf("size").forGetter(structure -> structure.size),
                     HeightProvider.CODEC.fieldOf("start_height").forGetter(structure -> structure.startHeight),
                     Heightmap.Type.CODEC.optionalFieldOf("project_start_to_heightmap").forGetter(structure -> structure.projectStartToHeightmap),
-                    Codec.intRange(1, 128).fieldOf("max_distance_from_center").forGetter(structure -> structure.maxDistanceFromCenter),
-                    DimensionPadding.CODEC.optionalFieldOf("dimension_padding", JigsawStructure.DEFAULT_DIMENSION_PADDING).forGetter(structure -> structure.dimensionPadding),
-                    StructureLiquidSettings.codec.optionalFieldOf("liquid_settings", JigsawStructure.DEFAULT_LIQUID_SETTINGS).forGetter(structure -> structure.liquidSettings)
+                    Codec.intRange(1, 128).fieldOf("max_distance_from_center").forGetter(structure -> structure.maxDistanceFromCenter)
             ).apply(instance, OceanStructures::new));
 
     private final RegistryEntry<StructurePool> startPool;
@@ -76,8 +68,6 @@ public class OceanStructures extends Structure {
     private final HeightProvider startHeight;
     private final Optional<Heightmap.Type> projectStartToHeightmap;
     private final int maxDistanceFromCenter;
-    private final DimensionPadding dimensionPadding;
-    private final StructureLiquidSettings liquidSettings;
 
     public OceanStructures(Structure.Config config,
                          RegistryEntry<StructurePool> startPool,
@@ -85,9 +75,7 @@ public class OceanStructures extends Structure {
                          int size,
                          HeightProvider startHeight,
                          Optional<Heightmap.Type> projectStartToHeightmap,
-                         int maxDistanceFromCenter,
-                         DimensionPadding dimensionPadding,
-                         StructureLiquidSettings liquidSettings)
+                         int maxDistanceFromCenter)
     {
         super(config);
         this.startPool = startPool;
@@ -96,8 +84,6 @@ public class OceanStructures extends Structure {
         this.startHeight = startHeight;
         this.projectStartToHeightmap = projectStartToHeightmap;
         this.maxDistanceFromCenter = maxDistanceFromCenter;
-        this.dimensionPadding = dimensionPadding;
-        this.liquidSettings = liquidSettings;
     }
 
     /*
@@ -177,10 +163,7 @@ public class OceanStructures extends Structure {
                         // Here at projectStartToHeightmap, blockpos's y value is 60 which means the structure spawn 60 blocks above terrain height.
                         // Set projectStartToHeightmap to be empty optional for structure to be place only at the passed in blockpos's Y value instead.
                         // Definitely keep this an empty optional when placing structures in the nether as otherwise, heightmap placing will put the structure on the Bedrock roof.
-                        this.maxDistanceFromCenter, // Maximum limit for how far pieces can spawn from center. You cannot set this bigger than 128 or else pieces gets cutoff.
-                        StructurePoolAliasLookup.EMPTY, // Optional thing that allows swapping a template pool with another per structure json instance. We don't need this but see vanilla JigsawStructure class for how to wire it up if you want it.
-                        this.dimensionPadding, // Optional thing to prevent generating too close to the bottom or top of the dimension.
-                        this.liquidSettings); // Optional thing to control whether the structure will be waterlogged when replacing pre-existing water in the world.
+                        this.maxDistanceFromCenter); // Maximum limit for how far pieces can spawn from center. You cannot set this bigger than 128 or else pieces gets cutoff.
 
         /*
          * Note, you are always free to make your own StructurePoolBasedGenerator class and implementation of how the structure
