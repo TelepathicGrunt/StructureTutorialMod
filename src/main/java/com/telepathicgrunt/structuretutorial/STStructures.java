d161c6544baa39c772ff02e23395a34eddede106
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2025-04-11 18:12:20 -0400

    FIx backport

--- src/main/java/com/telepathicgrunt/structuretutorial/STStructures.java
+++ src/main/java/com/telepathicgrunt/structuretutorial/STStructures.java
@@ -14,7 +14,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class STStructures {
 
     /**
-     * We are using the Deferred Registry system to register our structure as this is the preferred way on NeoForge.
+     * We are using the Deferred Registry system to register our structure as this is the preferred way on Forge.
      * This will handle registering the base structure for us at the correct time so we don't have to handle it ourselves.
      */
     public static final DeferredRegister<StructureType<?>> DEFERRED_REGISTRY_STRUCTURE = DeferredRegister.create(Registries.STRUCTURE_TYPE, StructureTutorialMain.MODID);
